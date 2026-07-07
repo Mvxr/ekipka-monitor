1c6b66ed30c2f73427e90294c801a87c02db0d90
Mvxr <mic.hadrys@gmail.com>
2026-07-08 01:00:55 +0200

    update

--- script.js
+++ script.js
@@ -1,51 +1,32 @@
 async function loadStaff(){
 
-const response =
-await fetch("staff.json");
+    const response = await fetch("/staff.json");
 
+    const users = await response.json();
 
-const users =
-await response.json();
+    const box = document.getElementById("staff");
 
+    box.innerHTML = "";
 
-const box =
-document.getElementById("staff");
+    users.forEach(user => {
 
+        box.innerHTML += `
+        <div class="member">
 
-box.innerHTML="";
+            <img src="${user.avatar}">
 
+            <a href="${user.profile}" target="_blank">
+                ${user.name}
+            </a>
 
-users.forEach(user=>{
+        </div>
+        `;
 
+    });
 
-box.innerHTML += `
-
-<div class="member">
-
-<img src="${user.avatar}">
-
-
-<a href="${user.profile}" target="_blank">
-
-${user.name}
-
-</a>
-
-
-</div>
-
-`;
-
-});
-
-
-document.getElementById("status").innerHTML =
-"Aktywna ekipa: "
-+ users.length
-+ " osób";
-
+    document.getElementById("status").innerHTML =
+    "Aktywna ekipa: " + users.length + " osób";
 
 }
-
 
 loadStaff();
